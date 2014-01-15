0b7575ead02d3631326da5fa1d811acb4962a8f1
Joris de Jong <joris@hybridvisuals.nl>
2014-01-15 19:34:45 +0100

    Adding Traer library

---
+++ ofxTraerPhysics/README.md
@@ -0,0 +1,39 @@
+ofxTraerPhysics 3.0.1
+=====================
+Adaption from libcinder version of Traer Physics to dependencies less openframeworks version.
+
+Introduction
+------------
+Creates a easy to setup Particle System which can create particles and particle-to-particle forces. Original in Java but very popular for Processing, so many of you might used it with Processing.
+
+Licence
+-------
+Licensed under the same "do whatever you want just email Jeff" license as the
+Java/Processing version.
+
+See http://murderandcreate.com/physics/ for the original Java code and examples.
+
+See also https://github.com/bloomtime/CinderTraer for the c++ port this was based upon for cinder.
+
+Installation
+------------
+Just drop the folder into the `openFrameworks/addons/` folder. Or git clone. After you have done that I've made it easier to just include the ofxTraerPhysics.h which includes all the necessary headers.
+
+Dependencies
+------------
+none
+
+Compatibility
+------------
+0.7.4
+
+Known issues
+------------
+There seems to be an issue with Particle Attraction sometimes. They [particles] jiggle a bit to much.
+
+Version history
+------------
+
+
+### Version 3.0.1 (Date): 2013-02-25
+Initial release w/ openframeworks version 0.7.4
---
+++ ofxTraerPhysics/src/Particle.cpp
@@ -0,0 +1,57 @@
+
+#include "Particle.h"
+
+namespace traer { namespace physics {
+
+    //--------------------------------------------------------------
+    Particle::Particle(){
+        mass = 1.0;
+        age = 0;
+        fixed = false;
+        locked = false;
+        position.set(0,0,0);
+        velocity.set(0,0,0);
+        force.set(0,0,0);
+        radius = 10.0;
+    }
+
+    //--------------------------------------------------------------
+    Particle::Particle( const float &m, const float &r )
+    {
+        mass = m;
+        age = 0;
+        fixed = false;
+		locked = false;
+        position.set(0,0,0);
+        velocity.set(0,0,0);
+        force.set(0,0,0);
+        radius = r;
+    }
+
+    //--------------------------------------------------------------
+    Particle::~Particle(){
+
+    }
+
+    //--------------------------------------------------------------
+    void Particle::makeFixed(){
+    
+        // makes particle fixed
+        fixed = true;        
+        
+    };
+
+    //--------------------------------------------------------------
+    void Particle::reset()
+    {
+        mass = 1.0f;
+        age = 0;
+        fixed = false;
+		locked = false;
+        position.set(0,0,0);
+        velocity.set(0,0,0);
+        force.set(0,0,0);
+        radius = 10.0;
+    }
+
+} } // namespace traer::physics
